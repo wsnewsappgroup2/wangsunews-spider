43ef3d80396a0943f58c5923992263656d668938
wangyq1 <wangyq1@wangsu.com>
2019-08-06 01:56:36 +0800

    待添加和dao层的连接

--- src/main/java/com/crow/controller/LoginController.java
+++ src/main/java/com/crow/controller/LoginController.java
@@ -1,15 +1,13 @@
 package com.crow.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.crow.entity.User;
 import com.crow.service.LoginService;
-import com.crow.utils.HttpClientUtil;
-import com.crow.utils.WechatApiUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -21,42 +19,23 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @PostMapping("/wsnews/login")
-    public String user_login(@RequestParam("code") String code){
-
-        // 授权校验登录
-        JSONObject jsonObject = JSONObject.parseObject(loginService.varifyByWechat(code));
-
-        // 保存用户信息
-        loginService.saveUserIfNotExists(jsonObject);
-
-        return "OK";
-
-//        // 根据返回的user实体类，判断用户是否是新用户，不是的话，更新最新登录时间，是的话，将用户信息存到数据库
-//        User user = userService.selectByOpenId(open_id);
-//        if(user != null){
-//            user.setUserNewLogin(new Date());
-//            userService.updateById(user);
-//        }else{
-//            User insert_user = new User();
-//            insert_user.setUserHead(userHead);
-//            insert_user.setUserName(userName);
-//            insert_user.setUserGender(userGender);
-//            insert_user.setUserNewLogin(new Date());
-//            insert_user.setUserCity(userCity);
-//            insert_user.setUserProvince(userProvince);
-//            insert_user.setUserOpenid(open_id);
-//            System.out.println("insert_user:"+insert_user.toString());
-//            // 添加到数据库
-//            Boolean flag = userService.insert(insert_user);
-//            if(!flag){
-//                return new JsonResult(ResultCode.FAIL);
-//            }
-//        }
-//        // 封装返回小程序
-//        Map<String, String> result = new HashMap<>();
-//        result.put("session_key", session_key);
-//        result.put("open_id", open_id);
-//        return new JsonResult(ResultCode.SUCCESS, result);
+    @PostMapping(value = "/wsnews/login",consumes = "application/json",produces = "application/json;charset=UTF-8")
+    public String loginByWechatAccount(@RequestBody Map<String, String> map){
+        // 用户登录
+        User user=loginService.login(map.get("code"),map.get("username"));
+        String msg="Login Failed";
+        String openid="-1";
+        if(user==null){
+            // 授权登录失败
+        }else{
+            // 成功
+            openid=user.getOpenid();
+            msg="Login Success";
+        }
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("open_id",openid);
+        jsonObject.put("msg",msg);
+        String res=jsonObject.toJSONString();
+        return res;
     }
 }
--- src/main/java/com/crow/service/LoginService.java
+++ src/main/java/com/crow/service/LoginService.java
@@ -1,6 +1,8 @@
 package com.crow.service;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.crow.entity.User;
 import com.crow.utils.HttpClientUtil;
 import com.crow.utils.WechatApiUtil;
 import org.springframework.stereotype.Service;
@@ -11,7 +13,39 @@ import java.util.Map;
 @Service
 public class LoginService {
 
-    public String varifyByWechat(String code){
+
+    public User login(String code,String username){
+        // 向微信请求授权登录
+        JSONObject validateResult= validateByWechat(code);
+        // 依照不同的响应码做处理
+        int errorCode=validateResult.getIntValue("errcode");
+        User user=null;
+        switch (errorCode){
+            case 0:
+                // 授权请求成功
+                validateResult.put("username",username);
+                user= getUserByValidateResult(validateResult);
+                if(user!=null)
+                    break;
+            case 40029:
+                // 无效的临时登录凭证 code
+                // break;
+            case -1:
+                // 系统繁忙，此时请开发者稍候再试
+                // break;
+            case 45011:
+                // 频率限制，每个用户每分钟100次
+                // break;
+            default:
+                // 异常响应码
+                break;
+        }
+        return user;
+    }
+
+
+    /**向微信请求授权登录**/
+    private JSONObject validateByWechat(String code){
         // 配置请求参数
         Map<String, String> param = new HashMap<>();
         param.put("appid", WechatApiUtil.WX_APP_ID);
@@ -21,11 +55,45 @@ public class LoginService {
 
         // 向微信API发送请求
         String wxResult = HttpClientUtil.doGet(WechatApiUtil.WX_AUTH_CODE2SESSION_URL, param);
-        return wxResult;
+        JSONObject jsonObject=JSONObject.parseObject(wxResult);
+        return jsonObject;
     }
 
-    // 保存授权的用户的数据
-    public void saveUserIfNotExists(JSONObject object){
+    /**获取授权的用户信息，如果是初次使用则会先保存用户信息**/
+    private User getUserByValidateResult(JSONObject validateResult){
+        String openid=validateResult.getString("openid");
+        String session_key=validateResult.getString("session_key");
+        String unionid=validateResult.getString("unionid");
 
+        User user=null;
+        if(exists(openid)){
+            user=getUserByOpenId(openid);
+        }else{
+            user= JSON.toJavaObject(validateResult,User.class);
+        }
+        return user;
+    }
+
+    /**判断用户是否在数据库中**/
+    private boolean exists(String openid){
+        // TODO: 完善判断语句
+        return false;
+    }
+
+    /**从数据库中获取对应的用户信息**/
+    private User getUserByOpenId(String openid){
+        // TODO:修改逻辑为正确逻辑并和dao层对接
+        User user=new User();
+        user.setOpenid("test_openid_123");
+        user.setSession_key("test_session_key_123");
+        user.setUnionid("test_unionid_123");
+        user.setUsername("test_username_123");
+        return user;
+    }
+
+    private void saveUserByValidateResult(User user){
+        // TODO:修改逻辑为正确逻辑并和dao层对接
+        System.out.println("Saved Successfully");
+        return;
     }
 }
