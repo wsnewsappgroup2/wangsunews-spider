c535d8be8fc9ef439fb80d39c792c464ca2eb9f4
SerenaWo <11923455843@qq.com>
2019-08-06 12:40:51 +0800

    改动数据库配置，修改虎扑新闻时间抓取

--- src/test/java/com/crow/HupuspiderApplicationTests.java
+++ src/test/java/com/crow/HupuspiderApplicationTests.java
@@ -1,20 +1,26 @@
 package com.crow;
 
+import com.crow.controller.StartSpiderController;
+import com.crow.dao.NewsListMapper;
+import com.crow.entity.CommentDetail;
+import com.crow.entity.NewsList;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HupuspiderApplicationTests {
+	@Autowired
+	StartSpiderController startSpiderController;
 
 	@Test
-	public void contextLoads() {
-		String word = "欢迎/v";
-		if(word.matches("[\\u4e00-\\u9fa5]+/(n|v|a)")) {
-			System.out.println(word.substring(0,word.length() - 2));
-		}
+	public void testFunc(){
+		String result = startSpiderController.index();
+		Assert.assertSame("爬虫开启", result);
 	}
 
 }
