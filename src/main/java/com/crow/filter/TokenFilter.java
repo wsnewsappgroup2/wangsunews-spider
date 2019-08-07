713235a02cca124c2f6f8d4b29070e1e891f42a2
wangyq1 <wangyq1@wangsu.com>
2019-08-07 16:10:14 +0800

    过滤器+token鉴权

--- src/main/java/com/crow/filter/TokenFilter.java
+++ src/main/java/com/crow/filter/TokenFilter.java
@@ -1,5 +1,7 @@
 package com.crow.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.crow.result.ResultInfo;
 import com.crow.utils.JwtUtil;
 import org.springframework.http.HttpStatus;
 
@@ -8,17 +10,19 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 
 /**
  * @ClassName:XflameFilter
- * @Author: wuy2
+ * @Author: wuy2 wangyq1
  * @Description: DONE
  * @Date: Created in 9:38 2019/8/7
  * @Version: V1.0
  */
-@WebFilter
+@WebFilter()
 public class TokenFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -32,35 +36,77 @@ public class TokenFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        String token = httpServletRequest.getHeader("Authorization");
-        if(!JwtUtil.verify(token)){
-            this.response401(servletRequest, servletResponse, "token过期");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //设置允许跨域的配置
+        // 这里填写你允许进行跨域的主机ip（正式上线时可以动态配置具体允许的域名和IP）
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods","POST, GET, PUT, OPTIONS, DELETE, PATCH");
+        // Access-Control-Max-Age 用于 CORS 相关配置的缓存
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers","token,Origin, X-Requested-With, Content-Type, Accept");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+
+        ResultInfo resultInfo=new ResultInfo();
+        String token = request.getHeader("Authorization");
+        boolean isValidate=false;
+
+
+        String method=request.getMethod();
+        if(method.equals("OPTIONS")){
+            // 如果是预请求方法则直接放过
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(request,response);
+        }else{
+            if(token!=null && !token.isEmpty() && JwtUtil.verify(token)){
+                resultInfo.setSuccess(true);
+                resultInfo.setMsg("用户token验证授权通过");
+                isValidate=true;
+            }else{
+                resultInfo.setMsg("用户token验证授权失败，请求头的Authorization中没有token信息或token信息无效");
+            }
         }
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+        if(!isValidate){
+            response401(servletRequest, servletResponse, resultInfo);
+        }else{
+            filterChain.doFilter(request, response);
+        }
     }
 
     /**
      * 无需转发，直接返回Response信息
      */
-    private void response401(ServletRequest req, ServletResponse resp, String msg) {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) resp;
+    private void response401(ServletRequest request, ServletResponse response, ResultInfo resultInfo) throws UnsupportedEncodingException,IOException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-        httpServletResponse.setCharacterEncoding("UTF-8");
-        httpServletResponse.setContentType("application/json; charset=utf-8");
-        PrintWriter out = null;
-        //之后构造错误返回的code和status
+
+        PrintWriter printWriter = null;
+        OutputStreamWriter outputStreamWriter = null;
+
         try {
-            out = httpServletResponse.getWriter();
-//            String data = JsonUtil.toJson(new Result(false, HttpStatus.UNAUTHORIZED.value(), "无权访问(Unauthorized):" + msg, null));  写入登录失败的返回信息
-//            out.append(data);
+            outputStreamWriter = new OutputStreamWriter(response.getOutputStream(),
+                    "UTF-8");
+            printWriter = new PrintWriter(outputStreamWriter, true);
+            String jsonStr = JSON.toJSONString(resultInfo);
+            printWriter.write(jsonStr);
+            printWriter.flush();
+            printWriter.close();
+            outputStreamWriter.close();
+        } catch (UnsupportedEncodingException e) {
+            throw e;
+            //logger.error("过滤器返回信息失败:" + e.getMessage(), e);
         } catch (IOException e) {
-//            LOGGER.error("直接返回Response信息出现IOException异常:" + e.getMessage());
-//            throw new CustomException("直接返回Response信息出现IOException异常:" + e.getMessage());
+            throw e;
+            //logger.error("过滤器返回信息失败:" + e.getMessage(), e);
         } finally {
-            if (out != null) {
-                out.close();
+            if (null != printWriter) {
+                printWriter.close();
+            }
+            if (null != outputStreamWriter) {
+                outputStreamWriter.close();
             }
         }
     }
