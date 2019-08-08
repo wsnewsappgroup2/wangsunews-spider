48dabf62372797221360fcc62f2dacbc3e8f5ab1
wangyq1 <wangyq1@wangsu.com>
2019-08-08 20:23:16 +0800

    测试应用是否启动的controller

---
+++ src/main/java/com/crow/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.crow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @ClassName:TestController
+ * @Author: wuy2
+ * @Description: DONE
+ * @Date: Created in 19:40 2019/8/8
+ * @Version: V1.0
+ */
+@Controller
+public class TestController {
+    @CrossOrigin
+    @GetMapping("/test")
+    public String test() {
+        return "wsnewsapp is running!";
+    }
+}
